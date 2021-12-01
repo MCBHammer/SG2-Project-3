d66845b884d42e5d251d8264c4f987a5ce5d7bcb
MCBHammer <armanaznavour@yahoo.com>
2021-12-01 01:22:08 -0600

    Working except for setting BS

---
+++ Assets/Game Stuff/Scripts/DebugText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugText : MonoBehaviour
+{
+    [SerializeField] PlayerData _playerData = null;
+    [SerializeField] EnemyData _enemyData = null;
+    [SerializeField] Text _text = null;
+
+    void Update()
+    {
+        _text.text = "Debug Text:" + "\n" + "Player Health: " + _playerData.PlayerHealth + "\n" + "Player Charges: " + _playerData.PlayerCharges + "\n"
+            + "Player Shields: " + _playerData.PlayerShields + "\n" + "Player Shield Cooldown: " + _playerData.PlayerShieldCooldown + "\n" +
+            "Enemy Health: " + _enemyData.EnemyHealth + "\n" + "Enemy Charges: " + _enemyData.EnemyCharges + "\n"
+            + "Enemy Shields: " + _enemyData.EnemyShields + "\n" + "Enemy Shield Cooldown: " + _enemyData.EnemyShieldCooldown;
+    }
+}
